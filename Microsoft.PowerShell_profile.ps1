d42c5d1972fb72b0a57bc66daea6ed1fefd45c82
PRIYAM SHARMA <priyamsharma2704@gmail.com>
2025-01-22 22:30:52 -0600

    Windows Powershell prompt customization and aliases

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,55 @@
+# from terminal type: code $PROFILE or notepad $PROFILE
+function gs
+{
+	git status
+}
+
+function ga
+{
+	git add .
+}
+
+function gc
+{
+	git commit -m "$args"
+}
+
+function next
+{
+	cd "C:\Dev\NextJS"
+}
+
+function react
+{
+	cd "C:\Dev\React"
+}
+
+function js
+{
+	cd "C:\Dev\JavaScript"
+}
+
+# Powershell Prompt customization
+function prompt {
+    $branch = git branch --show-current 2>$null
+    $gitStatus = $null
+    if ($branch) {
+        $gitStatus = git status -s
+        $uncommittedChanges = $gitStatus.Count
+    }
+
+    Write-Host "📁 " -NoNewline -ForegroundColor Blue
+    if ($branch) {
+        $branchColor = $uncommittedChanges -gt 0 ? "Yellow" : "Green"
+        Write-Host "$(Get-Location) " -NoNewline
+        Write-Host "🌿 " -NoNewline -ForegroundColor Green
+        Write-Host "[$branch]" -ForegroundColor Cyan -NoNewline
+        if ($uncommittedChanges -gt 0) {
+            Write-Host " [+$uncommittedChanges]" -NoNewline -ForegroundColor Yellow
+        }
+        return " > "
+    }
+    else {
+        return "$(Get-Location) > "
+    }
+}
